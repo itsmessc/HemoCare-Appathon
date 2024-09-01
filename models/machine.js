27ae11b00fad5266b53aa2b594f3e46a1eed4d43
itsmessc <ssaicharan2212@gmail.com>
2024-09-01 16:13:45 +0530

    "Added enum constraint to 'status' field in MachineSchema"

--- models/machine.js
+++ models/machine.js
@@ -4,7 +4,7 @@ const MachineSchema = new mongoose.Schema({
   manufacturing_serial_number: { type: String, required: true },
   machine_type: { type: String, required: true },
   location: { type: String, required: true },
-  status: { type: String, required: true }, 
+  status: { type: String, required: true, enum: ['Vacant', 'Occupied', 'Maintenance', 'Under Repair'] }, 
   start_time: { type: Date },
   end_time: { type: Date},
 },{timestamps:true});
